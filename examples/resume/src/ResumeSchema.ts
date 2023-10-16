ac59a14be57e04795214160dddff17206cce2dd4
Frederic Luchting <github@luchting.de>
2023-10-16 14:43:38 +0200

    Add simple resume example

--- examples/resume/src/ResumeSchema.ts
+++ examples/resume/src/ResumeSchema.ts
@@ -1,5 +1,29 @@
 // The following is a schema definition for determining the resume of a some user input.
 
-export interface ResumeResponse {
-    sentiment: "negative" | "neutral" | "positive";  // The sentiment of the text
+export interface Resume {
+    header: string;  // An appealing title for the resume
+    experiences: Experience[];
+    educations: Education[];
+    skills: Skill[];
 }
+
+// work experience at a company with a given role
+export interface Experience{
+    startDate: string;
+    companyName: string;
+    role: string;
+}
+
+// education of the candidate form university or certification
+export interface Education{
+    instituationName: string;
+    examTitle: string;
+    year: string;
+}
+
+// Skills of the person with a level of professionalism
+export interface Skill{
+    name: string;
+    level: number;
+}
+
--- examples/resume/src/main.ts
+++ examples/resume/src/main.ts
@@ -2,14 +2,19 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 import { createLanguageModel, createJsonTranslator, processRequests } from "typechat";
-import { ResumeResponse } from "./ResumeSchema";
+import { Resume } from "./resumeSchema";
 
 // TODO: use local .env file.
 dotenv.config({ path: path.join(__dirname, "../../../.env") });
 
 const model = createLanguageModel(process.env);
 const schema = fs.readFileSync(path.join(__dirname, "resumeSchema.ts"), "utf8");
-const translator = createJsonTranslator<ResumeResponse>(model, schema, "ResumeResponse");
+const translator = createJsonTranslator<Resume>(model, schema, "Resume");
+
+function processResume(resume: Resume) {
+    // Process the items in the cart
+    void resume;
+}
 
 // Process requests interactively or from the input file specified on the command line
 processRequests("RM> ", process.argv[2], async (request) => {
@@ -18,5 +23,8 @@ processRequests("RM> ", process.argv[2], async (request) => {
         console.log(response.message);
         return;
     }
-    console.log(`The resume is ${response.data.resume}`);
+    const resume = response.data;
+    console.log(JSON.stringify(resume, undefined, 2));
+    processResume(resume);
+    console.log("Success!");
 });
